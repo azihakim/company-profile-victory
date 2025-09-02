5898a380944b96ecb8f74c3fa6d9af0ef1615e52
Muhammad Abdul Azizul Hakim <113509874+azihakim@users.noreply.github.com>
2025-09-02 21:44:10 +0700

    navbar

---
+++ scripts.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {
+    const navbar = document.getElementById("navbar");
+
+    if (window.pageYOffset > 0) {
+        navbar.classList.add("sticky"); // Tambahkan kelas sticky saat discroll
+    } else {
+        navbar.classList.remove("sticky"); // Hapus kelas sticky saat di atas
+    }
+};
